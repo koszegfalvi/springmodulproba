62e145d216282dc9a11e8db914018873d374bf77
koszegfalvi <128134080+koszegfalvi@users.noreply.github.com>
2023-10-11 17:14:33 +0200

    Initial commit

---
+++ src/main/java/hu/progmatic/springmodulproba/SpringmodulprobaApplication.java
@@ -0,0 +1,13 @@
+package hu.progmatic.springmodulproba;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringmodulprobaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringmodulprobaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/hu/progmatic/springmodulproba/SpringmodulprobaApplicationTests.java
@@ -0,0 +1,13 @@
+package hu.progmatic.springmodulproba;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringmodulprobaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
